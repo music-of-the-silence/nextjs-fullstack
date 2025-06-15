aed87773be58f0cb6b3c94ca18f88df835b55047
Chandan Nath <144007351+music-of-the-silence@users.noreply.github.com>
2025-06-16 01:50:40 +0530

    Backend Setup

---
+++ lib/auth.ts
@@ -0,0 +1,74 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "./db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Email",
+          required: true,
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+          required: true,
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email or password not found");
+        }
+        try {
+          await connectToDatabase();
+          const user = await User.findOne({ email: credentials.email });
+
+          if (!user) {
+            throw new Error("No user found with this email");
+          }
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (!isValid) {
+            throw new Error("Invalid Password");
+          }
+          return { id: user._id.toString(), email: user.email };
+        } catch (error) {
+          console.error("Error in logging in:", error);
+          throw new Error("Failed to log in");
+        }
+      },
+    }),
+  ],
+  calllbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as a string;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+    error: "/login",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  secret:process.env.NEXTAUTH_SECRET,
+};
